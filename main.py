5c7458c1e7231b70b8b24e4c4cb9b01cfee0f044
Felipe Nunes <rennasccenth@gmail.com>
2019-04-11 18:02:25 -0400

    Introdução do menu de emprestimos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from views.clienteView import viewCliente
 from views.livroView import viewLivro
+from views.emprestimoView import viewEmprestimo
 
 
 
@@ -8,13 +9,15 @@ while True:
 
     # TODO: Logica aqui
 
-    opcao = int(input("[1]Livro [2]Cliente [3]Sair\n=>"))
+    opcao = int(input("[1]Livro [2]Cliente [3]Emprestimo [4]Sair\n=>"))
 
     if opcao == 1:
         viewLivro()
     elif opcao == 2:
         viewCliente()
     elif opcao == 3:
+        viewEmprestimo()
+    elif opcao == 4:
 
         print("Saindo...")
         break
